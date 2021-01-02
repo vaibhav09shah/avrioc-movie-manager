86447d33cd95a625c84af77a72f7fef20575f296
unknown <vaibhav09shah@gmail.com>
2021-01-02 12:12:36 +0530

    Deployment releated changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Adding Deployment & Global Prefix for API
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
